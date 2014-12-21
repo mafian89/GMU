8f647f271a6fe1dab50605e250eda849f09a205a
Tomas Mikulica <mafian89@gmail.com>
2014-12-21 15:36:48 +0100

    NDK

---
+++ Android/OpenGLESTest/jni/ShadersUtils.h
@@ -0,0 +1,22 @@
+/*
+ * ShadersUtils.h
+ *
+ *  Created on: 21.12.2014
+ *      Author: Tomas
+ */
+#include "Common.h"
+
+#ifndef SHADERSUTILS_H_
+#define SHADERSUTILS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SHADERSUTILS_H_ */
